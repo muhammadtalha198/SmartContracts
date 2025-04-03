8ffca34ffba7863e99ec0aeedc14df9e97af5969
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-04-03 15:00:39 +0500

    Update token.sol

--- Moonshot megaX/Moonshot_smartontracts/token.sol
+++ Moonshot megaX/Moonshot_smartontracts/token.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Moonshot is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __ERC20_init("Moonshot MAGAX", "$MAGAX");
+        __ERC20Burnable_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+
+        _mint(initialOwner, 1000000000000 * 1e18);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+}
